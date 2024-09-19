4510fa0d807ccfd4db16aa05f43fb987a1d1a010
ironshine <cjfgml2320@gmail.com>
2024-09-19 15:49:16 +0900

    first commit

---
+++ src/main/java/com/sparta/outsourcing_team_project/OutsourcingTeamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.outsourcing_team_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutsourcingTeamProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutsourcingTeamProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/outsourcing_team_project/OutsourcingTeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.outsourcing_team_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutsourcingTeamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
