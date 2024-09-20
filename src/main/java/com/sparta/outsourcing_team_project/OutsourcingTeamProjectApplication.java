58a65136bf4da8f73e6c162eeb1a35d5b672153c
ironshine <cjfgml2320@gmail.com>
2024-09-20 22:01:51 +0900

    feat: conflicts 해결을 위한 발버둥

--- src/main/java/com/sparta/outsourcing_team_project/OutsourcingTeamProjectApplication.java
+++ src/main/java/com/sparta/outsourcing_team_project/OutsourcingTeamProjectApplication.java
@@ -2,13 +2,8 @@ package com.sparta.outsourcing_team_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
-
-import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class OutsourcingTeamProjectApplication {
 
     public static void main(String[] args) {
