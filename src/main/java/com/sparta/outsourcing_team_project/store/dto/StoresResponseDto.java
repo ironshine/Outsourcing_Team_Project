ca49d449ba8702f0bfef7d5dd0d5b5f05452a4de
ironshine <cjfgml2320@gmail.com>
2024-09-22 20:05:38 +0900

    feat: 광고 구현
    - Store builder 패턴 적용
    - StoreService stream 사용하여 코드 간결화
    - 메서드 파라미터값 변경하여 코드 간결화
    - 오픈,마감 시간 String -> LocalTime 변경
    - 최소주문금액 String -> Integer 변경
    -  래퍼클래스 -> 원시타입 변경(Entity 제외)
    - 누적 광고비 추가
    - 광고비 추가 기능 구현
    - 광고비 음수 입력 못하게 예외처리

--- src/main/java/com/sparta/outsourcing_team_project/store/dto/StoresResponseDto.java
+++ src/main/java/com/sparta/outsourcing_team_project/store/dto/StoresResponseDto.java
@@ -1,30 +1,32 @@
 package com.sparta.outsourcing_team_project.store.dto;
 
+import com.sparta.outsourcing_team_project.store.entity.Store;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Getter
 public class StoresResponseDto {
     private final Long id;
     private final String storeName;
-    private final String storeOpenTime;
-    private final String storeCloseTime;
-    private final String minOrderPrice;
+    private final LocalTime storeOpenTime;
+    private final LocalTime storeCloseTime;
+    private final int minOrderPrice;
     private final Boolean storeStatus;
     private final LocalDateTime createAt;
     private final LocalDateTime updatedAt;
-//    private final Long userId;
+    private final Long userId;
 
-    public StoresResponseDto(Long id, String storeName, String storeOpenTime, String storeCloseTime, String minOrderPrice, Boolean storeStatus, LocalDateTime createAt, LocalDateTime updatedAt){ //, Long userId) {
-        this.id = id;
-        this.storeName = storeName;
-        this.storeOpenTime = storeOpenTime;
-        this.storeCloseTime = storeCloseTime;
-        this.minOrderPrice = minOrderPrice;
-        this.storeStatus = storeStatus;
-        this.createAt = createAt;
-        this.updatedAt = updatedAt;
-//        this.userId = userId;
+    public StoresResponseDto(Store store) {
+        this.id = store.getId();
+        this.storeName = store.getStoreName();
+        this.storeOpenTime = store.getStoreOpenTime();
+        this.storeCloseTime = store.getStoreCloseTime();
+        this.minOrderPrice = store.getMinOrderPrice();
+        this.storeStatus = store.getStoreStatus();
+        this.createAt = store.getCreateAt();
+        this.updatedAt = store.getUpdatedAt();
+        this.userId = store.getUser().getId();
     }
 }
