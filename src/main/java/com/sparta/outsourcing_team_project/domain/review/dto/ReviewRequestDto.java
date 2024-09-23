1fd03427b279eeed640e3f9e2078fe4dd797eb2a
taehyeok Nam <navyth91@naver.com>
2024-09-23 16:20:45 +0900

    Revert "feat: 리뷰 작성 및 조회(미완성)"
    This reverts commit 5ce7139a464a5dceee42c8d4a25c74067d88f8a5.

--- src/main/java/com/sparta/outsourcing_team_project/domain/review/dto/ReviewRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.outsourcing_team_project.domain.review.dto;
-
-import lombok.Getter;
-
-@Getter
-public class ReviewRequestDto {
-     private String content;
-     private int rating;
-}
