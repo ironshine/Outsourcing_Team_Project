e21a0831c5c7cd5eec40ba02708b04c3300a2d72
SpringStudy <dnjsdyd78@gmail.com>
2024-09-23 20:36:16 +0900

    feat: aop 오류수정, 접근검증로직추가, 예외문핸들링구현, enum클래스 입력데이터 검증로직추가
    이하동문

--- src/main/java/com/sparta/outsourcing_team_project/domain/order/aop/AopConfig.java
+++ src/main/java/com/sparta/outsourcing_team_project/domain/order/aop/AopConfig.java
@@ -1,10 +1,15 @@
 package com.sparta.outsourcing_team_project.domain.order.aop;
 
+import com.sparta.outsourcing_team_project.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@RequiredArgsConstructor
 @Configuration
 public class AopConfig {
+
+    private UserRepository userRepository;
 
     @Bean
     public LoggingAspect getAspect(){
