c63c7fb34701807a8a53e79d41391a52709f4a49
SpringStudy <dnjsdyd78@gmail.com>
2024-09-23 20:48:11 +0900

    feat: aop 오류수정
    앗 실수 ㅋ

--- src/main/java/com/sparta/outsourcing_team_project/domain/order/aop/AopConfig.java
+++ src/main/java/com/sparta/outsourcing_team_project/domain/order/aop/AopConfig.java
@@ -1,18 +1,22 @@
 package com.sparta.outsourcing_team_project.domain.order.aop;
 
 import com.sparta.outsourcing_team_project.domain.user.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@RequiredArgsConstructor
 @Configuration
 public class AopConfig {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public AopConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Bean
-    public LoggingAspect getAspect(){
-        return new LoggingAspect();
+    public LoggingAspect getAspect() {
+        return new LoggingAspect(userRepository);
     }
 }
