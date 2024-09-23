fb487c90566a0ce4946694bb4bf4bbcc1465cada
KUGOM <yaeji0728@naver.com>
2024-09-23 15:00:06 +0900

    feat: ArgumentResolver추가
    ArgumentResolve와 wedconfig를 추가

---
+++ src/main/java/com/sparta/outsourcing_team_project/config/ArgumentResolver.java
@@ -0,0 +1,44 @@
+package com.sparta.outsourcing_team_project.config;
+
+import com.sparta.outsourcing_team_project.domain.auth.exception.AuthException;
+import com.sparta.outsourcing_team_project.domain.common.annotation.Auth;
+import com.sparta.outsourcing_team_project.domain.common.dto.AuthUser;
+import com.sparta.outsourcing_team_project.domain.user.enums.UserRole;
+import io.micrometer.common.lang.Nullable;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class ArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasAuthAnnotation = parameter.getParameterAnnotation(Auth.class) != null;
+        boolean isAuthUserType = parameter.getParameterType().equals(AuthUser.class);
+
+        if(hasAuthAnnotation != isAuthUserType) {
+            throw new AuthException("@Auth와 AuthUser 타입은 함께 사용되어야 합니다.");
+        }
+        return hasAuthAnnotation;
+    }
+
+    @Override
+    public Object resolveArgument(
+            @Nullable MethodParameter parameter,
+            @Nullable ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            @Nullable WebDataBinderFactory binderFactory
+    ){
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+
+        Long userId = (Long) request.getAttribute("userId");
+        String userEmail = (String) request.getAttribute("userEmail");
+        UserRole userRole = UserRole.of((String) request.getAttribute("userRole"));
+
+        return new AuthUser(userId, userEmail, userRole);
+    }
+}
+
---
+++ src/main/java/com/sparta/outsourcing_team_project/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.sparta.outsourcing_team_project.config;
+
+import com.sparta.outsourcing_team_project.domain.common.annotation.Auth;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new ArgumentResolver());
+    }
+}
