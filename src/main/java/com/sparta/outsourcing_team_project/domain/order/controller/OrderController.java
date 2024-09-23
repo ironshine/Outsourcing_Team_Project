ab0c139bbef68980188fdb2089cc35d39af79e7b
SpringStudy <dnjsdyd78@gmail.com>
2024-09-23 18:44:22 +0900

    feat: 주문 요청 및 주문 상태 변경시 요청 로그 출력 기능 구현
    주문 요청, 주문 수락, 주문 상태 변경시 로그에 요청자의 가게id, 주문id 출력 기능 추가
    ps) User 파트 연관관계 설정되면 데이터조회로직 수정필요

--- src/main/java/com/sparta/outsourcing_team_project/domain/order/controller/OrderController.java
+++ src/main/java/com/sparta/outsourcing_team_project/domain/order/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.sparta.outsourcing_team_project.domain.order.controller;
 
 import com.sparta.outsourcing_team_project.domain.common.annotation.Auth;
 import com.sparta.outsourcing_team_project.domain.common.dto.AuthUser;
+import com.sparta.outsourcing_team_project.domain.order.aop.annotation.RequestTrack;
 import com.sparta.outsourcing_team_project.domain.order.dto.OrderOptionsRequestDto;
 import com.sparta.outsourcing_team_project.domain.order.dto.OrderOptionsResponseDto;
 import com.sparta.outsourcing_team_project.domain.order.dto.OrderResponseDto;
@@ -21,7 +22,7 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    //주문 메뉴 옵션 조회
+    // 주문 메뉴 옵션 조회
     @GetMapping("/storeId/{storeId}/menuId/{menuId}")
     public ResponseEntity<List<OrderOptionsResponseDto>> getMenuOptions(
             @Auth AuthUser authUser,
@@ -29,7 +30,8 @@ public class OrderController {
             @PathVariable Long menuId) {
         return ResponseEntity.ok(orderService.getMenuOptions(storeId, menuId, authUser));
     }
-
+    // 주문 요청
+    @RequestTrack
     @PostMapping
     public ResponseEntity<OrderResponseDto> createOrder(
             @RequestBody OrderOptionsRequestDto requestDto, @Auth AuthUser authUser) {
@@ -37,12 +39,16 @@ public class OrderController {
         return ResponseEntity.ok(orderService.createOrder(requestDto, authUser));
     }
 
+    // 주문 수락
+    @RequestTrack
     @PatchMapping("/{orderId}")
     public ResponseEntity<OrderStatusResponseDto> approveOrder(@PathVariable Long orderId, @Auth AuthUser authUser) {
 
         return ResponseEntity.ok(orderService.acceptOrder(orderId, authUser));
     }
 
+    // 주문 상태 변경
+    @RequestTrack
     @PatchMapping
     public ResponseEntity<OrderStatusResponseDto> changeOrderStatus(
             @RequestParam Long storeId,
