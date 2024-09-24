104d45643921c79967ae2f0d74e580e52a5e8970
Your Name <doolchong@gmail.com>
2024-09-25 03:46:29 +0900

    refactor: 패키지 변경으로 인한 import 문제 해결

--- src/test/java/com/sparta/outsourcing_team_project/domain/menu/optiongroup/controller/OptionGroupControllerTest.java
+++ src/test/java/com/sparta/outsourcing_team_project/domain/menu/optiongroup/controller/OptionGroupControllerTest.java
@@ -21,84 +21,84 @@ import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@MockBean(JpaMetamodelMappingContext.class)
-//@WebMvcTest(OptionGroupController.class)
-//class OptionGroupControllerTest {
-//
-//    @MockBean
-//    private OptionGroupService optionGroupService;
-//
-//    @Autowired
-//    private OptionGroupController optionGroupController;
-//
-//    private MockMvc mockMvc;
-//
-//    @MockBean
-//    private ArgumentResolver argumentResolver;
-//
-//    @BeforeEach
-//    public void setup() {
-//        mockMvc = MockMvcBuilders.standaloneSetup(optionGroupController)
-//                .setCustomArgumentResolvers(argumentResolver)
-//                .build();
-//    }
-//
-//    @Nested
-//    class create {
-//
-//        @Test
-//        public void 옵션_그룹_생성_정상동작() throws Exception {
-//            //given
-//            ObjectMapper objectMapper = new ObjectMapper();
-//
-//            given(argumentResolver.supportsParameter(any())).willReturn(true);
-//            given(argumentResolver.resolveArgument(any(), any(), any(), any())).willReturn(TEST_AUTH_USER_OWNER);
-//            given(optionGroupService.create(any(), anyLong(), any())).willReturn(TEST_OPTION_GROUP_RESPONSE_1);
-//
-//            //when
-//            ResultActions result = mockMvc.perform(post("/api/stores/menus/{menuId}", TEST_ID_1)
-//                    .contentType("application/json")
-//                    .content(objectMapper.writeValueAsString(TEST_OPTION_GROUP_REQUEST_1)));
-//
-//            //then
-//            result.andExpect(status().isOk());
-//        }
-//    }
-//
-//    @Nested
-//    class update {
-//
-//        @Test
-//        public void 옵션_그룹_수정_정상동작() throws Exception {
-//            //given
-//            ObjectMapper objectMapper = new ObjectMapper();
-//
-//            given(argumentResolver.supportsParameter(any())).willReturn(true);
-//            given(argumentResolver.resolveArgument(any(), any(), any(), any())).willReturn(TEST_AUTH_USER_OWNER);
-//            given(optionGroupService.update(any(), anyLong(), anyLong(), any())).willReturn(TEST_OPTION_GROUP_RESPONSE_1);
-//
-//            //when
-//            ResultActions result = mockMvc.perform(put("/api/stores/menus/{menuId}/option-groups/{optionGroupId}", TEST_ID_1, TEST_ID_1)
-//                    .contentType("application/json")
-//                    .content(objectMapper.writeValueAsString(TEST_OPTION_GROUP_REQUEST_1)));
-//
-//            //then
-//            result.andExpect(status().isOk());
-//        }
-//    }
-//
-//    @Nested
-//    class delete {
-//
-//        @Test
-//        public void 옵션_그룹_삭제_정상동작() throws Exception {
-//            //given
-//
-//            //when
-//            ResultActions result = mockMvc.perform(delete("/api/stores/menus/option-groups/{optionGroupId}", TEST_ID_1));
-//
-//            //then
-//            result.andExpect(status().isOk());
-//        }
-//    }
-//}
+@MockBean(JpaMetamodelMappingContext.class)
+@WebMvcTest(OptionGroupController.class)
+class OptionGroupControllerTest {
+
+    @MockBean
+    private OptionGroupService optionGroupService;
+
+    @Autowired
+    private OptionGroupController optionGroupController;
+
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ArgumentResolver argumentResolver;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(optionGroupController)
+                .setCustomArgumentResolvers(argumentResolver)
+                .build();
+    }
+
+    @Nested
+    class create {
+
+        @Test
+        public void 옵션_그룹_생성_정상동작() throws Exception {
+            //given
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            given(argumentResolver.supportsParameter(any())).willReturn(true);
+            given(argumentResolver.resolveArgument(any(), any(), any(), any())).willReturn(TEST_AUTH_USER_OWNER);
+            given(optionGroupService.create(any(), anyLong(), any())).willReturn(TEST_OPTION_GROUP_RESPONSE_1);
+
+            //when
+            ResultActions result = mockMvc.perform(post("/api/stores/menus/{menuId}", TEST_ID_1)
+                    .contentType("application/json")
+                    .content(objectMapper.writeValueAsString(TEST_OPTION_GROUP_REQUEST_1)));
+
+            //then
+            result.andExpect(status().isOk());
+        }
+    }
+
+    @Nested
+    class update {
+
+        @Test
+        public void 옵션_그룹_수정_정상동작() throws Exception {
+            //given
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            given(argumentResolver.supportsParameter(any())).willReturn(true);
+            given(argumentResolver.resolveArgument(any(), any(), any(), any())).willReturn(TEST_AUTH_USER_OWNER);
+            given(optionGroupService.update(any(), anyLong(), anyLong(), any())).willReturn(TEST_OPTION_GROUP_RESPONSE_1);
+
+            //when
+            ResultActions result = mockMvc.perform(put("/api/stores/menus/{menuId}/option-groups/{optionGroupId}", TEST_ID_1, TEST_ID_1)
+                    .contentType("application/json")
+                    .content(objectMapper.writeValueAsString(TEST_OPTION_GROUP_REQUEST_1)));
+
+            //then
+            result.andExpect(status().isOk());
+        }
+    }
+
+    @Nested
+    class delete {
+
+        @Test
+        public void 옵션_그룹_삭제_정상동작() throws Exception {
+            //given
+
+            //when
+            ResultActions result = mockMvc.perform(delete("/api/stores/menus/option-groups/{optionGroupId}", TEST_ID_1));
+
+            //then
+            result.andExpect(status().isOk());
+        }
+    }
+}
--- src/test/java/com/sparta/outsourcing_team_project/domain/menu/optiongroup/option/service/OptionServiceTest.java
+++ src/test/java/com/sparta/outsourcing_team_project/domain/menu/optiongroup/option/service/OptionServiceTest.java
@@ -1,6 +1,6 @@
 package com.sparta.outsourcing_team_project.domain.menu.optiongroup.option.service;
 
-import com.sparta.outsourcing_team_project.domain.common.exception.InvalidRequestException;
+import com.sparta.outsourcing_team_project.config.exception.InvalidRequestException;
 import com.sparta.outsourcing_team_project.domain.menu.optiongroup.option.dto.OptionResponse;
 import com.sparta.outsourcing_team_project.domain.menu.optiongroup.option.repository.OptionRepository;
 import com.sparta.outsourcing_team_project.domain.menu.optiongroup.repository.OptionGroupRepository;
