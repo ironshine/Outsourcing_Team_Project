3b942a3b5c2b56922400258d5b3dcc96eacac732
ironshine <cjfgml2320@gmail.com>
2024-09-25 21:32:45 +0900

    feat: 글로벌 익셉션에 익셉션 추가
    - properties에 spring.web.resources.add-mappings=false 추가
    - api 잘못입력 예외
    - 글로벌 익셉션외의 익셉션 예외

--- src/main/java/com/sparta/outsourcing_team_project/config/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/outsourcing_team_project/config/exception/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.nio.file.AccessDeniedException;
 import java.util.Objects;
@@ -44,6 +45,15 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(HttpStatus.BAD_REQUEST + " :  " + ex.getMessage());
     }
 
+    // API 잘못입력
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<String> noHandlerFoundException(NoHandlerFoundException ex) {
+        ex.getRequestURL();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                HttpStatus.NOT_FOUND + " :  요청하신 API를 찾을 수 없습니다. 입력 값을 다시 확인 해주세요.\n"
+                        + "현재 입력 값 : " + ex.getHttpMethod() + " " + ex.getRequestURL());
+    }
+
     // @Valid 할때 유효성 검사 실패
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> validationExceptions(MethodArgumentNotValidException ex) {
@@ -64,8 +74,14 @@ public class GlobalExceptionHandler {
                 + Objects.requireNonNull(ex.getRequiredType()).getSimpleName()
                 + " 타입으로 정확히 입력해주세요."
                 + " 현재 입력값은 "
-                + ex.getValue()
-                + " 입니다.";
+                + ex.getValue().getClass().getSimpleName()
+                + " 타입 입니다.";
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(HttpStatus.BAD_REQUEST + " :  " + errorMessage);
+    }
+    // 모든 예외 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        String errorMessage = "알 수 없는 오류가 발생했습니다 : " + ex.getMessage();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
     }
 }
